7aa2d985b514abde25032bf1c88b835b6d00e5cd
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-10-07 17:34:32 -0500

    se instalan librerias necesaría y se crea el archivo principal

---
+++ config/default.ts
@@ -0,0 +1,11 @@
+// mas adelante vemos las varibles de entorno 
+// para evitar que vean la contraseña
+
+
+export default {
+    host : "localhost2",
+    port: "3306",
+    user: "root",
+    password: "0987654321w",
+    database:"tienda_virtual"
+};
