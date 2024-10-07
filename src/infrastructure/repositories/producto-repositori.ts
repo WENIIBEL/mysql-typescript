13faacfe274b43b3629be30b4041335abdf90a66
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-10-07 18:05:09 -0500

    se crea el archivo producto-repositori para manipular la BD

---
+++ src/infrastructure/repositories/producto-repositori.ts
@@ -0,0 +1,9 @@
+// aqui interactuamos con la base de datos+
+import { productos } from "../../domain/models/producto";
+export class productoRepositori {
+
+    agregarProducto(producto:productos) {
+
+    }
+
+}
