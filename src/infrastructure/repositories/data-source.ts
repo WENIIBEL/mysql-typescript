4c69d592affdaaed93e910f54b7bff0bd7e5c322
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-10-10 12:06:45 -0500

    se comentan los archivos, metodos para manipulas BD

--- src/infrastructure/repositories/data-source.ts
+++ src/infrastructure/repositories/data-source.ts
@@ -1,8 +1,9 @@
 //crea la conexión con la BD
 
-import mysql from 'mysql2/promise'; // esto es una libreria externa
+import mysql from 'mysql2/promise'; // esto es una libreria externa a la BD
 import  config from "../../../config/default";
 
+// crea la coneción a las BD
 export const getPoolConnection = async () => {
     const connection = mysql.createPool({
         host: config.host,
