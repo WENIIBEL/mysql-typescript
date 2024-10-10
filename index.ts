85ae97780b2c70dea22190da1d3259289f5c1eb5
wendy bolivar <125563457+WENIIBEL@users.noreply.github.com>
2024-10-10 12:21:11 -0500

    llamamos obtener productos en el index

--- index.ts
+++ index.ts
@@ -1,20 +1,24 @@
-import { ProductoRepositori } from "./src/infrastructure/repositories/Producto-repositori"; 
+import { ProductoRepositori } from "./src/infrastructure/repositories/Producto-repositori";
 import { Producto } from "./src/domain/models/Producto";
 
 // la clase que tiene acceso a las base de datos
 const productoRepositori = new ProductoRepositori();
 
-// agregamos los productos
-const producto1 = new Producto ({
-    nombre: "televisor LG",
-    descripcion: "televisor LG 40 pulgadas",
-    precio: 1000000,
-    cantidad_disponible: 10,
-})
+
 
 const main = async () => {
+    // agregamos los productos
+    const producto1 = new Producto({
+        nombre: "televisor LG",
+        descripcion: "televisor LG 40 pulgadas",
+        precio: 1000000,
+        cantidad_disponible: 10,
+    })
     const result = await productoRepositori.agregarProducto(producto1)
     console.log(result);
+
+    const productos = await productoRepositori.obtenerProductos()
+    console.log(productos);
 }
 
 main();
